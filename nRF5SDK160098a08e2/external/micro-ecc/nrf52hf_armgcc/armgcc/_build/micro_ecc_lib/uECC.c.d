e3ce1f0df1c9ae5fec83017ff64793e564599307
tomcatty <thomascooney89@hotmail.com>
2020-09-02 19:05:48 +1000

    Compiled uECC by running "make" in \external\micro-ecc\nrf52hf_armgcc\armgcc

---
+++ nRF5SDK160098a08e2/external/micro-ecc/nrf52hf_armgcc/armgcc/_build/micro_ecc_lib/uECC.c.d
@@ -0,0 +1,38 @@
+_build/micro_ecc_lib/uECC.c.o: ../../micro-ecc/uECC.c \
+ ../../micro-ecc/uECC.h \
+ /Users/thomascooney/Documents/BluetrackHub/nRF5SDK160098a08e2/external_tools/gcc-arm-none-eabi-4_9-2015q3/lib/gcc/arm-none-eabi/4.9.3/include/stdint.h \
+ /Users/thomascooney/Documents/BluetrackHub/nRF5SDK160098a08e2/external_tools/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/stdint.h \
+ /Users/thomascooney/Documents/BluetrackHub/nRF5SDK160098a08e2/external_tools/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/machine/_default_types.h \
+ /Users/thomascooney/Documents/BluetrackHub/nRF5SDK160098a08e2/external_tools/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/sys/features.h \
+ /Users/thomascooney/Documents/BluetrackHub/nRF5SDK160098a08e2/external_tools/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/sys/_intsup.h \
+ /Users/thomascooney/Documents/BluetrackHub/nRF5SDK160098a08e2/external_tools/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/sys/_stdint.h \
+ ../../micro-ecc/uECC_vli.h ../../micro-ecc/types.h \
+ ../../micro-ecc/platform-specific.inc ../../micro-ecc/asm_arm.inc \
+ ../../micro-ecc/asm_arm_mult_square_umaal.inc \
+ ../../micro-ecc/curve-specific.inc
+
+../../micro-ecc/uECC.h:
+
+/Users/thomascooney/Documents/BluetrackHub/nRF5SDK160098a08e2/external_tools/gcc-arm-none-eabi-4_9-2015q3/lib/gcc/arm-none-eabi/4.9.3/include/stdint.h:
+
+/Users/thomascooney/Documents/BluetrackHub/nRF5SDK160098a08e2/external_tools/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/stdint.h:
+
+/Users/thomascooney/Documents/BluetrackHub/nRF5SDK160098a08e2/external_tools/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/machine/_default_types.h:
+
+/Users/thomascooney/Documents/BluetrackHub/nRF5SDK160098a08e2/external_tools/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/sys/features.h:
+
+/Users/thomascooney/Documents/BluetrackHub/nRF5SDK160098a08e2/external_tools/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/sys/_intsup.h:
+
+/Users/thomascooney/Documents/BluetrackHub/nRF5SDK160098a08e2/external_tools/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/sys/_stdint.h:
+
+../../micro-ecc/uECC_vli.h:
+
+../../micro-ecc/types.h:
+
+../../micro-ecc/platform-specific.inc:
+
+../../micro-ecc/asm_arm.inc:
+
+../../micro-ecc/asm_arm_mult_square_umaal.inc:
+
+../../micro-ecc/curve-specific.inc:
