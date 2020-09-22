47941408958036e9d0480b437ec1ee0d00607a20
tomcatty <thomascooney89@hotmail.com>
2020-09-22 21:19:28 +1000

    Connection settings supported 6 concurrent connections.

--- nRF5SDK160098a08e2/examples/ble_peripheral/experimental/ble_app_multiperipheral_sandbox/main.c
+++ nRF5SDK160098a08e2/examples/ble_peripheral/experimental/ble_app_multiperipheral_sandbox/main.c
@@ -84,15 +84,15 @@
 #define RADIO_LED                       BSP_BOARD_LED_3         
 #define LEDBUTTON_BUTTON                BSP_BUTTON_0                            /**< Button that will trigger the notification event with the LED Button Service */
 
-#define APP_ADV_INTERVAL                MSEC_TO_UNITS(20, UNIT_0_625_MS)        /**< The advertising interval (in units of 0.625 ms; this value corresponds to 40 ms). */
+#define APP_ADV_INTERVAL                MSEC_TO_UNITS(152.5, UNIT_0_625_MS)        /**< The advertising interval (in units of 0.625 ms; this value corresponds to 40 ms). */
 #define APP_ADV_DURATION                BLE_GAP_ADV_TIMEOUT_GENERAL_UNLIMITED   /**< The advertising duration (180 seconds) in units of 10 milliseconds. */
 
-#define MIN_CONN_INTERVAL               MSEC_TO_UNITS(150, UNIT_1_25_MS)         /**< Minimum acceptable connection interval (0.5 seconds). */
-#define MAX_CONN_INTERVAL               MSEC_TO_UNITS(300, UNIT_1_25_MS)        /**< Maximum acceptable connection interval (1 second). */
+#define MIN_CONN_INTERVAL               MSEC_TO_UNITS(165, UNIT_1_25_MS)         /**< Minimum acceptable connection interval (0.5 seconds). */
+#define MAX_CONN_INTERVAL               MSEC_TO_UNITS(180, UNIT_1_25_MS)        /**< Maximum acceptable connection interval (1 second). */
 #define SLAVE_LATENCY                   0                                       /**< Slave latency. */
-#define CONN_SUP_TIMEOUT                MSEC_TO_UNITS(4000, UNIT_10_MS)         /**< Connection supervisory time-out (4 seconds). */
+#define CONN_SUP_TIMEOUT                MSEC_TO_UNITS(1000, UNIT_10_MS)         /**< Connection supervisory time-out (4 seconds). */
 
-#define FIRST_CONN_PARAMS_UPDATE_DELAY  APP_TIMER_TICKS(20000)                  /**< Time from initiating event (connect or start of notification) to first time sd_ble_gap_conn_param_update is called (15 seconds). */
+#define FIRST_CONN_PARAMS_UPDATE_DELAY  APP_TIMER_TICKS(5000)                  /**< Time from initiating event (connect or start of notification) to first time sd_ble_gap_conn_param_update is called (15 seconds). */
 #define NEXT_CONN_PARAMS_UPDATE_DELAY   APP_TIMER_TICKS(5000)                   /**< Time between each call to sd_ble_gap_conn_param_update after the first call (5 seconds). */
 #define MAX_CONN_PARAMS_UPDATE_COUNT    3                                       /**< Number of attempts before giving up the connection parameter negotiation. */
 
@@ -370,7 +370,7 @@ static void on_connected(const ble_gap_evt_t * const p_gap_evt)
     ret_code_t  err_code;
     uint32_t    periph_link_cnt = ble_conn_state_peripheral_conn_count(); // Number of peripheral links.
 
-    NRF_LOG_INFO("Connection with link 0x%x established.", p_gap_evt->conn_handle);
+    NRF_LOG_INFO("Connection with link 0x%x established, max_conn_interval = 0x%x, minn_conn_interval = %x%x.", p_gap_evt->conn_handle, p_gap_evt->params.connected.conn_params.max_conn_interval, p_gap_evt->params.connected.conn_params.min_conn_interval);
 
     // Assign connection handle to available instance of QWR module.
     for (uint32_t i = 0; i < NRF_SDH_BLE_PERIPHERAL_LINK_COUNT; i++)
@@ -489,6 +489,10 @@ static void ble_evt_handler(ble_evt_t const * p_ble_evt, void * p_context)
                                              BLE_HCI_REMOTE_USER_TERMINATED_CONNECTION);
             APP_ERROR_CHECK(err_code);
             break;
+
+        case BLE_GAP_EVT_CONN_PARAM_UPDATE:
+            // Display new connection parameters
+        NRF_LOG_INFO("Connection with link 0x%x changed parameters to max_conn_interval = 0x%x, minn_conn_interval = %x%x.", p_ble_evt->evt.gap_evt.conn_handle, p_ble_evt->evt.gap_evt.params.connected.conn_params.max_conn_interval, p_ble_evt->evt.gap_evt.params.connected.conn_params.min_conn_interval);
 
         default:
             // No implementation needed.
@@ -683,7 +687,7 @@ int main(void)
     buttons_init();
     power_management_init();
     ble_stack_init();
-    err_code = radio_notification_init(3, NRF_RADIO_NOTIFICATION_TYPE_INT_ON_ACTIVE, NRF_RADIO_NOTIFICATION_DISTANCE_800US);
+    err_code = radio_notification_init(3, NRF_RADIO_NOTIFICATION_TYPE_INT_ON_BOTH, NRF_RADIO_NOTIFICATION_DISTANCE_800US);
     APP_ERROR_CHECK(err_code);
     gap_params_init();
     gatt_init();
