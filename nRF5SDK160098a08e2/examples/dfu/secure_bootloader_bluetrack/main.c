1a5803fac1c14130dcc8cd441cd40fb7b681f0b0
tomcatty <thomascooney89@hotmail.com>
2024-01-04 15:28:21 +1100

    Generated release artefacts (ensured .hex output not ignored), added licence, updated emProject for SES V7.32, removed HW signature protection

--- nRF5SDK160098a08e2/examples/dfu/secure_bootloader_bluetrack/main.c
+++ nRF5SDK160098a08e2/examples/dfu/secure_bootloader_bluetrack/main.c
@@ -37,6 +37,37 @@
  * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  */
+
+//  Copyright 2024 Thomas Cooney BSD-3-Clause
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//
+//  1. Redistributions of source code must retain the above copyright notice, this
+//  list of conditions and the following disclaimer.
+//
+//  2. Redistributions in binary form must reproduce the above copyright notice, this
+//  list of conditions and the following disclaimer in the documentation and/or
+//  other materials provided with the distribution.
+//
+//  3. Neither the name of the copyright holder nor the names of its contributors
+//  may be used to endorse or promote products derived from this software
+//  without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
+//  CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+//  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+//  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+//  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+//  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+//  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+//  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+//  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+//  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+//  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 /** @file
  *
  * @defgroup bootloader_secure_ble main.c
@@ -285,12 +316,14 @@ int main(void)
 
     // Calculate the signature.
     NRF_LOG_INFO("Verify hardware signature");
-    ret_val = nrf_crypto_ecdsa_verify(&verify_context,
+
+    // Remove hardware signature protection
+    ret_val = NRF_SUCCESS; /* nrf_crypto_ecdsa_verify(&verify_context,
                                        &public_key,
                                        hash,
                                        hash_len,
                                        signature,
-                                       NRF_CRYPTO_ECDSA_SECP256R1_SIGNATURE_SIZE);
+                                       NRF_CRYPTO_ECDSA_SECP256R1_SIGNATURE_SIZE); */
     if (ret_val != NRF_SUCCESS)
     {
         NRF_LOG_ERROR("Signature failed (err_code: 0x%x)", ret_val);
